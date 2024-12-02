3d1ee47f0cb1d0a2677165652c352fd9cdc87ac5
gabriel <ggabrielomar123@hotmail.com>
2024-12-02 13:16:29 -0400

    Adding category of product

--- src/Categories/categories.repository.ts
+++ src/Categories/categories.repository.ts
@@ -2,13 +2,11 @@ import { Injectable } from "@nestjs/common";
 import { Categories } from "./categories.entity";
 import { Repository } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Console } from "console";
 import { Products } from "../Products/products.entity";
 
 @Injectable()
 export class CategoriesRespository{
-    constructor(@InjectRepository(Categories) private categoriesDB:Repository<Categories>,
-  @InjectRepository(Products) private productsDB:Repository<Products>){}
+    constructor(@InjectRepository(Categories) private categoriesDB:Repository<Categories>){}
 
 
 
@@ -18,39 +16,30 @@ export class CategoriesRespository{
  }
 
 async addCategories(products: {name:string,description:string,
-  price:number, stock:number, category:string}[]): Promise<Categories[]>{
-    const arrCat = []
+  price:number, stock:number, category:string}[]): Promise<void>{
+
+    const arrCat = await this.categoriesDB.find();//[]
+    const listCat = []
+    console.log(arrCat)
+  //verify if the categories is already exist
+    if(arrCat && arrCat.length === 0){
     products.forEach((prod)=>{
-       if(arrCat === null){
-        arrCat.push(prod.category)
-       }else{
-        const catExist = arrCat.find((cat)=>{return cat === prod.category})
-        console.log(catExist)
-        if(!catExist){
-          arrCat.push(prod.category)
-        }
-       }
+
+        if(!listCat.includes(prod.category)){
+          listCat.push(prod.category)
+        }})
       
-      });  console.log("listCategory",arrCat)
-          try {
-            //let categoriasType= await this.categoriesDB.find();
-            console.log("2")
-            const categoriesNew =Promise.all(arrCat.map(async (cat)=>{//6
-                  console.log('1') 
-                  const createCategoria = await this.categoriesDB.create({name:cat});
-                  console.log("2rre",createCategoria)
-                   const cate = await this.categoriesDB.save(createCategoria)
-                    console.log("3",cate);
-
-                  return await cate;
-              }
-            ))
-            console.log('RepositoryReturn',categoriesNew)
-            return categoriesNew;
-
-          } catch (error) {
-            throw new Error('Error');
-          }
-     
-    }
+  // add the list of category
+    if(listCat.length>0){
+      try {
+        listCat.forEach(async (cat)=>{//6       
+              const createCategoria = await this.categoriesDB.create({name:cat});        
+                await this.categoriesDB.save(createCategoria)
+          })
+        console.log("New products registered")
+      } catch (error) {
+        throw new Error('Error');
+      }}}
+      console.log("the categories is already registereds")
+    };
   }
