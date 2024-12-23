83863a91ef3aee9279b55af1970d322ccc324c4a
gabriel <ggabrielomar123@hotmail.com>
2024-12-23 16:21:13 -0400

    config

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -3,22 +3,20 @@ import {config as dotenvConfig} from "dotenv";
 import { registerAs } from "@nestjs/config";
 
 dotenvConfig({path: 'src/.env.development'});
-
 const config={
       type: "postgres",//process.env.DB_NAME.
-      database: process.env.DBNAME,
-       host:process.env.DBHOST,
-       port: Number(process.env.DBPORT),
-       username: process.env.DBUSERNAME,
-       password: process.env.DBPASSWORD,
+      database: process.env.DBNAMER,
+       host:process.env.DBHOSTR,
+       port: Number(process.env.DBPORTR),
+       username: process.env.DBUSERNAMER,
+       password: process.env.DBPASSWORDR,
        autoLoadEntities:true,
        logging:true,
       // dropSchema:true,
-       synchronize:true,//sks
+      // synchronize:true,
        entities:['dist/**/*.entity{.js,.ts}'],
        migrations: ['dist/migrations/*{.js,.ts}'],//comand npm run migration:generate src/migrations/name
 }
-console.log("environment variable", process.env.DBHOST,process.env.DBNAME, process.env.DBPORT, process.env.DBUSERNAME, process.env.DBPASSWORD)
 export default registerAs('typeorms',()=> config)//REVISAR
 
 export const connectionSource = new DataSource(config as DataSourceOptions)
