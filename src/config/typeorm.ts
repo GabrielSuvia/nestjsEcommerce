43c474c11b60414d4a1ae6acfe36850c2d155151
gabriel <ggabrielomar123@hotmail.com>
2024-12-23 16:14:26 -0400

    deletes some directories

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -5,10 +5,10 @@ import { registerAs } from "@nestjs/config";
 dotenvConfig({path: 'src/.env.development'});
 
 const config={
-      type: "postgres",//process.env.DB_NAME
+      type: "postgres",//process.env.DB_NAME.
       database: process.env.DBNAME,
        host:process.env.DBHOST,
-       port: process.env.DBPORT,
+       port: Number(process.env.DBPORT),
        username: process.env.DBUSERNAME,
        password: process.env.DBPASSWORD,
        autoLoadEntities:true,
@@ -18,6 +18,7 @@ const config={
        entities:['dist/**/*.entity{.js,.ts}'],
        migrations: ['dist/migrations/*{.js,.ts}'],//comand npm run migration:generate src/migrations/name
 }
+console.log("environment variable", process.env.DBHOST,process.env.DBNAME, process.env.DBPORT, process.env.DBUSERNAME, process.env.DBPASSWORD)
 export default registerAs('typeorms',()=> config)//REVISAR
 
 export const connectionSource = new DataSource(config as DataSourceOptions)
