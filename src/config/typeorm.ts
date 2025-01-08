86191d6c9ab6716898ba22cc88646a713afb1412
gabriel <ggabrielomar123@hotmail.com>
2025-01-08 16:50:07 -0400

    l

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -4,15 +4,15 @@ import { registerAs } from "@nestjs/config";
 dotenvConfig({path: 'src/.env.development'});
 const config={
       type: "postgres",//process.env.DB_NAME.
-      database: process.env.DBNAMER,
-       host:process.env.DBHOSTR,
-       port: Number(process.env.DBPORTR),
-       username: process.env.DBUSERNAMER,
-       password: process.env.DBPASSWORDR,
+      database: process.env.DBNAME,
+       host:process.env.DBHOST,
+       port: Number(process.env.DBPORT),
+       username: process.env.DBUSERNAME,
+       password: process.env.DBPASSWORD,
        autoLoadEntities:true,
        logging:true,
-      // dropSchema:true,
-      synchronize:true,
+       dropSchema:true,
+      //synchronize:true,
        entities:['dist/**/*.entity{.js,.ts}'],
        migrations: ['dist/migrations/*{.js,.ts}'],//comand npm run migration:generate src/migrations/name
 }
